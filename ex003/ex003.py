8aa19a6c03dc1f2e09d6ed7696750afcf463c40d
Sergio Pereira <54996062+sergiohgp@users.noreply.github.com>
2020-03-06 13:55:53 -0500

    Add files via upload

---
+++ ex003/ex003.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+n1 = int(input('Write a number: '))
+n2 = int(input('Write one more number: '))
+print('The sum between {} and {} is {}!'.format(n1,n2,n1+n2))
