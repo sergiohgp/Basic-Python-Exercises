d025b78ce125c3a0590f523e87372dc2c03905f1
Sergio Pereira <54996062+sergiohgp@users.noreply.github.com>
2020-03-07 17:58:16 -0500

    Add files via upload

---
+++ ex007/ex007.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+g1 = float(input('Write your first grade: '))
+g2 = float(input('Write your second grade: '))
+a = (g1+g2)/2
+print('So, your first grade was {} and your second was {}, correct? Your average is {}.'.format(g1,g2,a))
