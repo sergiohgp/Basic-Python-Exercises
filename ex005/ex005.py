dad554e3207c6b74641ec6fbefe039a2a1d57e98
Sergio Pereira <54996062+sergiohgp@users.noreply.github.com>
2020-03-07 17:57:53 -0500

    Add files via upload

---
+++ ex005/ex005.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+n = int(input('Write a number: '))
+p = n-1
+s = n+1
+print("You choose {}. It's predecessor is {} and successor is {}.".format(n,p,s))
