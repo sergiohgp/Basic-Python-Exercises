a0c3927559fc4d3e9f7696617186c25fa8aadd9d
Sergio Pereira <54996062+sergiohgp@users.noreply.github.com>
2020-03-08 17:18:27 -0400

    Add files via upload

---
+++ ex009/ex009.py
@@ -0,0 +1,15 @@
+#Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+n = int(input('Write an integer number among 0 and 9: '))
+t0 = n*0
+t1 = n*1
+t2 = n*2
+t3 = n*3
+t4 = n*4
+t5 = n*5
+t6 = n*6
+t7 = n*7
+t8 = n*8
+t9 = n*9
+print('The multiplication table for your number is: \n{}x0:{}\n{}x1:{}\n{}x2:{}\n{}x3:{}\n{}x4:{}\n{}x5:{}\n{}x6:{}\n{}x7:{}\n{}x8:{}\n{}x9:{}'.format(n,t0,n,t1,n,t2,n,t3,n,t4,n,t5,n,t6,n,t7,n,t8,n,t9,n))
+
+
