5800163b1ae809179948e5df6ab4f520f2bd00d2
Sergio Pereira <54996062+sergiohgp@users.noreply.github.com>
2020-03-17 18:37:10 -0400

    Add files via upload

---
+++ ex018/ex018.py
@@ -0,0 +1,6 @@
+import math
+x = float(input('Write any angle: '))
+s = math.sin(x)
+c = math.cos(x)
+t = math.tan(x)
+print('The angle sin, cos and tan is, in order, {}, {} and {}.'.format(s, c, t))
